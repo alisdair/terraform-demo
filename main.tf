f4ab9195cb377fc1cf94a66d5c47e7e6f2fa12a7
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2018-12-17 14:59:38 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "foo" {
+  triggers {
+    always = "#{uuid()}"
+  }
+}
+
+resource "random_id" "random" {
+  keepers {
+    uuid = "${uuid()}"
+  }
+
+  byte_length = 12
+}
+
+output "random" {
+  value = "${random_id.random.hex}"
+}
